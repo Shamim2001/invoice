b45c666e5898fa934702f5260538a4bf2e831fb9
shamim2001 <shamimahmedridoy2016@gmail.com>
2022-01-28 00:41:23 +0600

    client factory & data seeder

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -4,17 +4,21 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ClientFactory extends Factory
-{
+class ClientFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array
      */
-    public function definition()
-    {
+    public function definition() {
         return [
-            //
+            'name'      => $this->faker->name(),
+            'username'  => $this->faker->unique()->userName(),
+            'email'     => $this->faker->unique()->safeEmail(),
+            'phone'     => $this->faker->phoneNumber,
+            'country'   => $this->faker->country,
+            'thambnail' => 'https://picsum.photos/300?random=' . rand( 1, 22334 ),
+
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {
+        User::create( [
+            'name'      => 'Shamim Ahmed',
+            'email'     => 'test@gmail.com',
+            'password'  => bcrypt( '123' ),
+            'thumbnail' => 'https://picsum.phptos/300',
+        ] );
+
+        Client::factory( 10 )->create();
     }
 }
